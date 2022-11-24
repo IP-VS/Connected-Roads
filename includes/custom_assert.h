6237d0d67ed8f364ce4cec6f87a2f939cb9dd308
Lion Kortlepel <development@kortlepel.com>
2022-11-24 12:11:51 +0100

    more fixes

--- includes/custom_assert.h
+++ includes/custom_assert.h
@@ -3,11 +3,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define assert_not_null(ptr)                                                                                       \
-    do {                                                                                                           \
-        bool _val = ptr;                                                                                           \
-        if (!_val) {                                                                                               \
-            printk("Assertion failed: %s is null in %s:%d (%s), aborting.\n", #ptr, __FILE__, __LINE__, __func__); \
-            abort();                                                                                               \
-        }                                                                                                          \
+#define assert_not_null(ptr)                                                          \
+    do {                                                                              \
+        bool _val = (ptr == NULL);                                                    \
+        if (_val) {                                                                   \
+            printk("Assertion failed: %s is null in %s:%d (%s), aborting.\r\n", #ptr, \
+                __FILE__, __LINE__, __func__);                                        \
+        }                                                                             \
+    } while (0)
+
+#define assert_equal(left, right)                                                                     \
+    do {                                                                                              \
+        bool _val = ((left) == (right));                                                              \
+        if (!_val) {                                                                                  \
+            printk("Assertion failed: %s == %s is false in %s:%d (%s), aborting.\r\n", #left, #right, \
+                __FILE__, __LINE__, __func__);                                                        \
+        }                                                                                             \
     } while (0)
