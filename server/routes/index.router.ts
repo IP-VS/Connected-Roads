4108bd37f2a95190ce6926748ae4d2d1c1f344c3
Eric <eric@bostrot.com>
2022-12-08 10:29:13 +0100

    Changed port & added start message

--- server/routes/index.router.ts
+++ server/routes/index.router.ts
@@ -2,14 +2,18 @@ import express from 'express';
 var router = express.Router();
 import ws from 'ws';
 import dotenv from 'dotenv'
-import {initSerial, serialport, connected} from '../services/serial.service';
+import { initSerial, serialport, connected } from '../services/serial.service';
 import { MeshNode, NodeList } from '../models/node.model';
 
 dotenv.config();
 
 /* Websocket server */
-const wsServer = new ws.Server({ port: 8080 });
+const wsServer = new ws.Server({ port: 5327 });
 initSerial(wsServer);
+// On listen
+wsServer.on('listening', () => {
+  console.log('Websocket server listening on port 5327');
+});
 wsServer.on('connection', socket => {
   socket.on('message', (message) => {
     console.log(message);
