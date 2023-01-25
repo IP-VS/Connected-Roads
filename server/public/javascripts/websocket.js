ff4ae60b0f97c20e99b8a64e103823bad37d343f
Eric <eric@bostrot.com>
2023-01-25 21:39:43 +0100

    Handle events in webinterface

--- server/public/javascripts/websocket.js
+++ server/public/javascripts/websocket.js
@@ -4,6 +4,8 @@ var wsUrl = 'ws://localhost:5327';
 var socket;
 var connected = false;
 var interval;
+var nodes;
+var nodeEvents = [];
 
 var socketListener = function () {
     // Connection opened
@@ -45,16 +47,59 @@ var socketListener = function () {
         }
         // Microphone data
         if (msg.startsWith('micdata_raw:')) {
-            log('Microphone data received: ' + msg.replace(/[^0-9,]/g, ''));
+            // log('Microphone data received: ' + msg.replace(/[^0-9,]/g, ''));
         }
         // Detector data
         if (msg.startsWith('micdata_detector:')) {
-            log('Detector data received: ' + msg.replace(/[^0-9,]/g, ''));
+            // log('Detector data received: ' + msg.replace(/[^0-9,]/g, ''));
+            // Received node list parse from json string
+            try {
+                var val = msg.split(':')[1];
+                var nodeid = val.split(',')[0];
+                val = val.split(',')[1];
+                val = parseFloat(val);
+                val = val > 0 ? "From Left" : "From Right";
+
+                var name = nodes[nodeid - 1].name;
+
+                console.log(nodes);
+
+                // Current time
+                var now = new Date();
+                // Create table row
+                var rowElement = document.createElement('tr');
+                rowElement.innerHTML = `
+                    <td>${val}</td>
+                    <td>${now}</td>
+                    <td>${name}</td>
+                `
+                // Append
+                document.getElementById('events').appendChild(rowElement);
+
+                // Scroll to bottom
+                document.getElementById('events').scrollTop = document.getElementById('events').scrollHeight;
+
+                // Add event to node
+                nodeEvents[name] = val;
+                setTimeout(() => {
+                    nodeEvents[name] = undefined;
+                }, 5000);
+            }
+            catch (e) {
+                // Do nothing
+            }
         }
 
         // Received node list parse from json string
         try {
-            var nodes = JSON.parse(msg);
+            nodes = JSON.parse(msg);
+            // Remove duplicates by id
+            nodes = nodes.filter((thing, index, self) =>
+                index === self.findIndex((t) => (
+                    t.id === thing.id
+                ))
+            )
+
             // Clear table
             document.getElementById('nodes').innerHTML = '';
             // Parse json string
@@ -68,12 +113,18 @@ var socketListener = function () {
                 buttonElement.innerText = 'Remove';
                 // Add script
                 buttonElement.setAttribute('onclick', 'rmNode("' + nodeName + '")');
+                // Set event
+                var event = "None";
+                if (nodeEvents[nodeName] != undefined) {
+                    event = nodeEvents[nodeName] > 0 ? '➡️🚗' : '🚗⬅️';
+                }
                 // Create table row
                 var rowElement = document.createElement('tr');
                 rowElement.id = nodeName;
                 rowElement.innerHTML = `
                     <td>${nodeName}</td>
                     <td>${nodeStatus}</td>
+                    <td id="event${nodeName}">${event}</td>
                     <td>${buttonElement.outerHTML}</td>
                 `
                 if (document.getElementById(nodeName) == null) {
